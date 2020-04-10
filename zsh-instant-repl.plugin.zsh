907b9508d9abd69391a074f5a7419ce471384159
Jan Dammshäuser <mail@jandamm.de>
2020-04-10 19:21:58 +0200

    Add passing through exit code of wrapped line-init

--- zsh-instant-repl.plugin.zsh
+++ zsh-instant-repl.plugin.zsh
@@ -1,9 +1,11 @@
 function _zsh-instant-repl_zle-line-init() {
 	# call wrapped zle-line-init if it exists
 	(( ! ${+widgets[._zsh-instant-repl_orig_zle-line-init]} )) || zle ._zsh-instant-repl_orig_zle-line-init $@
+	local ret=$?
 	if [[ $CONTEXT = start ]]; then
 		LBUFFER=$zle_prefix$LBUFFER
 	fi
+	return $ret
 }
 
 function prime-zle-prefix() {
